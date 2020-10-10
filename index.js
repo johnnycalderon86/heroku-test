5d3a78e1609886d640e9d89b8d9dffc026fceca0
Johnny <johnnycalderondeveloper@gmail.com>
2020-10-11 00:25:06 +0200

    alll

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('*', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening for request on port ${PORT}`);
 })
