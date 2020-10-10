89fbb256a3f389b489c7faf8997be3bf7779d409
Johnny <johnnycalderondeveloper@gmail.com>
2020-10-10 19:41:06 +0200

    changing graphqlurl

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ mongoose.connection.once('open', () => {
 
 
 
-app.use('/graphql-f1', graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }))
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('*', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
