2d754c4206fecd4b6db23aba2e1756be789c8ca8
Johnny <johnnycalderondeveloper@gmail.com>
2020-10-10 22:23:58 +0200

    .catch

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors())
 
 //connecting to mongoDB server
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.or615.mongodb.net/${dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.or615.mongodb.net/${dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => console.log(err))
 
 mongoose.connection.once('open', () => {
     console.log('connected to database');
